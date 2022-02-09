3b66208f6aba1befb437f46a33a935183931b452
Ciara <ciarazsnijders@gmail.com>
2022-02-09 17:45:39 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ciarasnijders/Meet"
           target="_blank"
           rel="noopener noreferrer"
         >
